bb33a385ed307d1ac913961cddbc24b281f32ba1
MoeMamdouh <mohamed.mamdouh.do@gmail.com>
2022-05-17 21:12:47 +0200

    init

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
+import algoliasearch from 'algoliasearch';
+import { InstantSearch } from 'react-instantsearch-native';
+import SearchBox from './src/SearchBox';
+import InfiniteHits from './src/InfiniteHits';
+
+const searchClient = algoliasearch(
+  'FHGWOFHPQW',
+  'a66b02a8ea7088a0dce0efdca9eb348a'
+);
+
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    backgroundColor: '#252b33',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+});
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.safe}>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <InstantSearch
+          searchClient={searchClient}
+          indexName="international_flat_products"
+        >
+          <SearchBox />
+          <InfiniteHits />
+        </InstantSearch>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/InfiniteHits.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import PropTypes from 'prop-types';
+import { connectInfiniteHits } from 'react-instantsearch-native';
+import Highlight from './Highlight';
+
+const styles = StyleSheet.create({
+  separator: {
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+  },
+  item: {
+    padding: 10,
+    flexDirection: 'column',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  titleText: {
+    fontWeight: 'bold',
+  },
+  desc: {
+    marginHorizontal: 20,
+  },
+});
+
+const InfiniteHits = ({ hits, hasMore, refineNext }) => (
+  <FlatList
+    data={hits}
+    keyExtractor={(item) => item.objectID}
+    ItemSeparatorComponent={() => <View style={styles.separator} />}
+    onEndReached={() => hasMore && refineNext()}
+    renderItem={({ item }) => (
+      <View style={styles.item}>
+        <Image
+          source={{ uri: item.small_image.url }}
+          style={{ height: 100, width: 100 }}
+        />
+
+        <View style={styles.desc}>
+          <Text style={styles.titleText}>
+            <Highlight attribute="sku" hit={item} />
+          </Text>
+          <Text>{item.objectID}</Text>
+          <Text>{item.name_en}</Text>
+          <Text>{item.name_ar}</Text>
+          <Text>{item.stores}</Text>
+        </View>
+      </View>
+    )}
+  />
+);
+
+InfiniteHits.propTypes = {
+  hits: PropTypes.arrayOf(PropTypes.object).isRequired,
+  hasMore: PropTypes.bool.isRequired,
+  refineNext: PropTypes.func.isRequired,
+};
+
+export default connectInfiniteHits(InfiniteHits);
